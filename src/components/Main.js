d0de5b515de71195d31e8a5e2e0cca33107e0d7e
Alexander Dolzhenko <raskalovalexander@gmail.com>
2021-12-10 06:50:10 +0300

    feat: Main.js is configured to get information about the user and his avatar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,37 @@
+import React from "react";
+import api from "../utils/Api";
+
 function Main(props) {
+  const [userName, setUserName] = React.useState("Жак-Ив Кусто");
+  const [userDescription, setUserDescription] = React.useState(
+    "Исследователь океана"
+  );
+  const [userAvatar, setUserAvatar] = React.useState();
+  React.useEffect(() => {
+    api
+      .getUserInfo()
+      .then((userDataResponse) => {
+        setUserName(userDataResponse.name);
+        setUserDescription(userDataResponse.about);
+        setUserAvatar(userDataResponse.avatar);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
   return (
     <main className="content">
       <section className="profile page__profile">
         <div className="profile__info">
           <div onClick={props.onEditAvatar} className="avatar-container">
-            <img
-              src="<%=require('./images/avatar.jpg')%>"
-              alt="Аватар"
-              className="profile__avatar"
-            />
+            <img src={userAvatar} alt="Аватар" className="profile__avatar" />
             <button className="profile__avatar-edit"></button>
           </div>
 
           <div className="profile__user">
-            <h1 className="profile__title"></h1>
-            <p className="profile__subtitle"></p>
+            <h1 className="profile__title">{userName}</h1>
+            <p className="profile__subtitle">{userDescription}</p>
             <button
               onClick={props.onEditProfile}
               className="profile__edit-button animation-button"
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,4 +1,4 @@
-export default class Api {
+class Api {
   constructor(options) {
     this._url = options.baseUrl;
     this._headers = options.headers;
@@ -8,7 +8,7 @@ export default class Api {
     return fetch(`${this._url}/cards`, {
       method: "GET",
       headers: this._headers,
-    }).then(res => this._getResponse(res));
+    }).then((res) => this._getResponse(res));
   }
 
   addCard(data) {
@@ -16,21 +16,21 @@ export default class Api {
       method: "POST",
       headers: this._headers,
       body: JSON.stringify(data),
-    }).then(res => this._getResponse(res));
+    }).then((res) => this._getResponse(res));
   }
 
   deleteCard(cardId) {
     return fetch(`${this._url}/cards/${cardId}`, {
       method: "DELETE",
       headers: this._headers,
-    }).then(res => this._getResponse(res));
+    }).then((res) => this._getResponse(res));
   }
 
   getUserInfo() {
     return fetch(`${this._url}/users/me`, {
       method: "GET",
       headers: this._headers,
-    }).then(res => this._getResponse(res));
+    }).then((res) => this._getResponse(res));
   }
 
   setUserInfo(userName, userAbout) {
@@ -41,7 +41,7 @@ export default class Api {
         name: userName,
         about: userAbout,
       }),
-    }).then(res => this._getResponse(res));
+    }).then((res) => this._getResponse(res));
   }
 
   setUserAvatar(userAvatar) {
@@ -51,14 +51,14 @@ export default class Api {
       body: JSON.stringify({
         avatar: userAvatar,
       }),
-    }).then(res => this._getResponse(res));
+    }).then((res) => this._getResponse(res));
   }
 
   changeLikeCard(cardId, isLiked) {
-      return fetch(`${this._url}/cards/likes/${cardId}`, {
-        method: isLiked ? "DELETE" : "PUT",
-        headers: this._headers,
-      }).then(res => this._getResponse(res));
+    return fetch(`${this._url}/cards/likes/${cardId}`, {
+      method: isLiked ? "DELETE" : "PUT",
+      headers: this._headers,
+    }).then((res) => this._getResponse(res));
   }
 
   _getResponse(response) {
@@ -68,3 +68,13 @@ export default class Api {
     return Promise.reject(`Ошибка: ${response.status}`);
   }
 }
+
+const api = new Api({
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-30",
+  headers: {
+    authorization: "314a6403-12f3-42b6-b8d1-07fca9f8b44c",
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
