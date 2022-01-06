c542d185c855d64366c34db4534ff4fb6f911b3c
Alexander Dolzhenko <raskalovalexander@gmail.com>
2022-01-06 15:47:53 +0300

    feat: added support like-dislike card, delete card

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,7 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 function Main(props) {
   const currentUser = React.useContext(CurrentUserContext);
   const [cards, setCards] = React.useState([]);
+
   React.useEffect(() => {
     api
       .getInitialCards()
@@ -17,12 +18,42 @@ function Main(props) {
       });
   }, []);
 
+  function handleCardLike(card) {
+    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+
+    api
+      .changeLikeCard(card._id, isLiked)
+      .then((newCard) => {
+        setCards((state) =>
+          state.map((c) => (c._id === card._id ? newCard : c))
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function handleCardDelete(card) {
+    api
+      .deleteCard(card._id)
+      .then((newCards) => {
+        setCards((state) => state.filter((c) => (c._id !== card._id)));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   return (
     <main className="content">
       <section className="profile page__profile">
         <div className="profile__info">
           <div onClick={props.onEditAvatar} className="avatar-container">
-            <img src={currentUser.avatar} alt="Аватар" className="profile__avatar" />
+            <img
+              src={currentUser.avatar}
+              alt="Аватар"
+              className="profile__avatar"
+            />
             <button className="profile__avatar-edit"></button>
           </div>
 
@@ -44,7 +75,13 @@ function Main(props) {
       </section>
       <section className="grid-photo page__grid-photo">
         {cards.map((card) => (
-          <Card onCardClick={props.onCard} key={card._id} card={card} />
+          <Card
+            onCardClick={props.onCard}
+            onCardLike={handleCardLike}
+            onCardDelete={handleCardDelete}
+            key={card._id}
+            card={card}
+          />
         ))}
       </section>
     </main>
